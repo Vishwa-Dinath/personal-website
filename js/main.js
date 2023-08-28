02184e37f5a4aae1649fec20a5fc6c4166ac97b4
VishwaDinath <vishwadinath97@gmail.com>
2023-08-28 23:45:28 +0530

    Contacts section has been completed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const occupationElm = $('#occupation');
 const texts = ['Full Stack Web Developer','Freelancer','Statistician'];
+const theme = $('#theme')
 
 let i = 0;
 let j = 0;
@@ -26,3 +27,27 @@ setInterval(()=>{
         }
     }
 },100)
+
+var r = document.querySelector(':root');
+
+let dark = true;
+
+// function myFunction_get() {
+//     var rs = getComputedStyle(r);
+//     alert("The value of --blue is: " + rs.getPropertyValue('--dark'));
+// }
+
+function myFunction_set(colour1,colour2) {
+    r.style.setProperty('--dark', colour1);
+    r.style.setProperty('--light', colour2);
+}
+
+theme.on('click',()=>{
+    if (dark){
+        myFunction_set('#aed6ef','#000000');
+        dark = false;
+    }else if (!dark){
+        myFunction_set('black','white');
+        dark = true;
+    }
+})
